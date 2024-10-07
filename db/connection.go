176d3f45d8a1d1cf613ff3aa0e741995a8f1f800
andrescr <jcordoba@teledolar.com>
2024-10-07 17:44:44 -0600

    Corrección en la conexión

--- db/connection.go
+++ db/connection.go
@@ -19,10 +19,10 @@ func DBConnection() {
 	port := 1433             // Puerto de SQL Server
 	user := "sa"             // Usuario
 	password := "Prueba001." // Contraseña
-	database := "tdusers"    // Nombre de la base de datos
+	database := "tdusers-go" // Nombre de la base de datos
 
 	// Crear la cadena de conexión
-	DSN = fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&encrypt=true&trustServerCertificate=true", user, password, server, port, database)
+	DSN = fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&encrypt=true&trustServerCertificate=true&tlsmin=1.0", user, password, server, port, database)
 
 	// Conectarse a la base de datos usando GORM y el driver de SQL Server
 	DB, err = gorm.Open(sqlserver.Open(DSN), &gorm.Config{})
